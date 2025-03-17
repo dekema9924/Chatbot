d6e41a6c44b73768e63549cf85ddd56294c2af4a
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-03-17 11:15:48 -0400

    struggling

--- backend/routes/loginroute.js
+++ backend/routes/loginroute.js
@@ -14,7 +14,7 @@ loginroute.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
     function (req, res) {
         // Successful authentication, redirect home.
-        res.redirect('https://67d839b66d951f3371902874--monumental-piroshki-3176da.netlify.app/gemini');
+        res.redirect('/gemini');
 });
 
 loginroute.get('/', (req,res)=>{
