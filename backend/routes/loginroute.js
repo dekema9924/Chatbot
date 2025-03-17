ae357b90a72c147da6737128e701e396f1f014b2
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-03-17 01:13:44 -0400

    changed url

--- backend/routes/loginroute.js
+++ backend/routes/loginroute.js
@@ -14,7 +14,7 @@ loginroute.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
     function (req, res) {
         // Successful authentication, redirect home.
-        res.redirect('https://effortless-pastelito-663bf8.netlify.app');
+        res.redirect('https://67d7af3cd38ae1e8b4d70fc8--famous-biscotti-f67277.netlify.app/');
 });
 
 loginroute.get('/', (req,res)=>{
